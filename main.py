ea700302b47eca1f51b2c664fd315ad560aea04e
SSN <42188654+sangeethsn@users.noreply.github.com>
2022-08-09 11:49:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+
+
+import random
+import hangman_words as hw
+import hangman_art as art
+
+
+stages=art.stages
+logo=art.logo
+print(logo)
+chosen_word = random.choice(hw.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"{guess} is already predicted")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"{guess} not matches with actual word, You lose one life")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+  #pick right picture based on the lives
+    print(stages[lives])
